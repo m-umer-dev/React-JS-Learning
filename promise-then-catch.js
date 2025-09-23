ed356fdee9dc6dedb2fddbdc9597ef1561f3cf63
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-09-24 00:49:08 +0500

    Promises then & catch

---
+++ promise-then-catch.js
@@ -0,0 +1,15 @@
+let p = new Promise((res, rej) => {
+  setTimeout(() => {
+    console.log("This is the Promise One");
+    // res("Data is fetched");
+    rej("Data is Not Fetch");
+  }, 3000);
+});
+
+console.log(p);
+p.then((value) => {
+  console.log(value);
+});
+p.catch((error) => {
+  console.log(error);
+});
