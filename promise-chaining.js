f738201f70af4e5f41cb13b211bf5c00140268a0
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-09-27 17:29:28 +0500

    Promise chaining with load script function

--- promise-chaining.js
+++ promise-chaining.js
@@ -1,23 +1,23 @@
-// let pro1 = new Promise((res, rej) => {
-//   console.log("This is promise one which is print after 3 sec");
-//   setTimeout(() => {
-//     res("200 Ok!");
-//   }, 3000);
-// });
-// pro1
-//   .then((value) => {
-//     console.log(value);
-//     let pro2 = new Promise((res, rej) => {
-//       res("301 ok!");
-//     });
-//     return pro2;
-//   })
-//   .then((value) => {
-//     console.log(value);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+let pro1 = new Promise((res, rej) => {
+  console.log("This is promise one which is print after 3 sec");
+  setTimeout(() => {
+    res("200 Ok!");
+  }, 3000);
+});
+pro1
+  .then((value) => {
+    console.log(value);
+    let pro2 = new Promise((res, rej) => {
+      res("301 ok!");
+    });
+    return pro2;
+  })
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 //Script load function using promise
 
@@ -25,16 +25,19 @@ let loadScript = (src) => {
   return new Promise((res, rej) => {
     let script = document.createElement("Script");
     script.src = src;
-    script.onload(() => {
+    document.body.appendChild(script);
+    script.onload = () => {
       res("Script is load");
-    });
-    script.onerror(() => {
+    };
+    script.onerror = () => {
       rej("Script is not laoded");
-    });
+    };
   });
 };
 
-let callScript = loadScript("https://localhost:3000/");
+let callScript = loadScript(
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+);
 callScript
   .then((value) => {
     console.log(value);
