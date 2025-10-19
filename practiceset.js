12831b9a5f0479641a29b173b21c28e4eda978e0
m-umer-dev <omaraamir89@gmail.com>
2025-10-19 18:14:23 +0500

    Practice All concepts

---
+++ practiceset.js
@@ -0,0 +1,50 @@
+// let loadScript = (sources) => {
+//   return new Promise((res, rej) => {
+//     let script = document.createElement("script");
+//     script.src = sources;
+//     document.body.appendChild(script);
+//     script.onload = () => {
+//       res("Script is loaded");
+//     };
+//     script.onerror = () => {
+//       rej("Script is Failed");
+//     };
+//   });
+// };
+
+// let scriptFunction = loadScript(
+//   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+// );
+// scriptFunction
+//   .then((value) => {
+//     console.log(value);
+//   })
+//   .catch((value) => {
+//     console.log(value);
+//   });
+
+let loadScript = async (sources) => {
+  return new Promise((res, rej) => {
+    let script = document.createElement("script");
+    script.src = sources;
+    document.body.appendChild(script);
+    script.onload = () => {
+      res("Script is loaded");
+    };
+    script.onerror = () => {
+      rej("Script is Failed");
+    };
+  });
+};
+
+let main = async () => {
+  try {
+    let a = await loadScript(
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    );
+    console.log(a);
+  } catch {
+    console.log("Not working");
+  }
+};
+main();
