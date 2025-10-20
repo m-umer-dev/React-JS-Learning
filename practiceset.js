bdeeaa772947db02acef23bfaeeb1fa7d85f85ed
m-umer-dev <omaraamir89@gmail.com>
2025-10-21 00:05:53 +0500

    New Program practice

--- practiceset.js
+++ practiceset.js
@@ -23,28 +23,46 @@
 //     console.log(value);
 //   });
 
-let loadScript = async (sources) => {
+// let loadScript = async (sources) => {
+//   return new Promise((res, rej) => {
+//     let script = document.createElement("script");
+//     script.src = sources;
+//     document.body.appendChild(script);
+//     script.onload = () => {
+//       res("Script is loaded");
+//     };
+//     script.onerror = () => {
+//       rej("Script is Failed");
+//     };
+//   });
+// };
+
+// let main = async () => {
+//   try {
+//     let a = await loadScript(
+//       "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+//     );
+//     console.log(a);
+//   } catch {
+//     console.log("Not working");
+//   }
+// };
+// main();
+
+async function timer() {
   return new Promise((res, rej) => {
-    let script = document.createElement("script");
-    script.src = sources;
-    document.body.appendChild(script);
-    script.onload = () => {
-      res("Script is loaded");
-    };
-    script.onerror = () => {
-      rej("Script is Failed");
-    };
+    setTimeout(() => {
+      rej("Error After 3 Sec...");
+    }, 3000);
   });
-};
+}
 
 let main = async () => {
   try {
-    let a = await loadScript(
-      "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    );
-    console.log(a);
+    console.log("Pass");
   } catch {
-    console.log("Not working");
+    let a = await timer();
+    console.log(a);
+    console.log("not");
   }
 };
-main();
