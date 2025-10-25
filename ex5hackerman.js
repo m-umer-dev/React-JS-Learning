a18529fe1773b161abc0fa354c9d82fab995908b
m-umer-dev <omaraamir89@gmail.com>
2025-10-25 16:57:36 +0500

    Practice Program 2

--- ex5hackerman.js
+++ ex5hackerman.js
@@ -1,1 +1,19 @@
-let arg = ["Initializing Hack Program...", "Hacking "];
+let arg = [
+  "Initializing Hack Program...",
+  "Hacking ummroro username...",
+  "username found ummroro01",
+  "Connecting to Facebook",
+];
+
+function bodydisplay(args) {
+  for (let index = 0; index < args.length; index++) {
+    const element = args[index];
+    setTimeout(() => {
+      let p = document.createElement("p");
+      p.textContent = element;
+      document.body.append(p);
+    }, 3000 * index);
+  }
+}
+
+bodydisplay(arg);
